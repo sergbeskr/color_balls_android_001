bc05173d1d5b5ca63f35a12011a5b7d2f338b6af
Anna <sergbeskr04081993@gmail.com>
2017-01-27 09:25:31 +0200

    savepoint

--- color_balls_android_001/Assets/scripts/shooting/ShootController.cs
+++ color_balls_android_001/Assets/scripts/shooting/ShootController.cs
@@ -16,7 +16,7 @@ public class ShootController : MonoBehaviour {
 	private Vector3 position;
 	private float distance;
 	private float forceSpeed = 50;
-	private float thirtyFive = 35;
+	private float thirtyFive = 15;
 	private float sergAngle;
 
 	IBulletsElement element;
@@ -37,7 +37,7 @@ public class ShootController : MonoBehaviour {
 					directionRay = emitter.TransformDirection (position - emitter.position);  
 					distance = Vector3.Distance(position, emitter.position);
 					sergAngle = Mathf.Atan(thirtyFive / Mathf.Sqrt(directionRay.x * directionRay.x + directionRay.z * directionRay.z));
-					Debug.Log("1010010101010010101 "+sergAngle);
+					Debug.Log("++++++++++++ "+sergAngle);
 					forceSpeed = Mathf.Sqrt((distance * 9.8f) / Mathf.Sin(2*sergAngle));
 					//forceSpeed = Mathf.Sqrt (9.8f * thirtyFive);
 					element.AddForce (directionRay, forceSpeed, thirtyFive);
